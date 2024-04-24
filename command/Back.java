bcbdcf0f9ee5cd11a3efcde51b58601ff6af0329
Tyler Tran <TylerTran@u.boisestate.edu>
2024-04-23 19:18:46 -0700

    Create Back.java

---
+++ command/Back.java
@@ -0,0 +1,31 @@
+package command;
+
+import window.*;
+
+public class Back implements Command{
+    Window window;
+    public Back(Window w){
+        window = w;
+    }
+
+    @Override
+    public boolean undoable() {
+        return true;
+    }
+
+    @Override
+    public void execute() {
+        InsertPoint.getInsertPoint().back();
+    }
+
+    @Override
+    public void unexecute() {
+        InsertPoint.getInsertPoint().forward();
+    }
+
+    @Override
+    public Command clone(){
+        return new Back(window);
+    }
+
+}
