05e3ec916b958e1931b612649f7c5d7449ad565c
Tyler Tran <tjtranwok@gmail.com>
2024-04-23 19:21:35 -0700

    Create InsertCharGlyph.java

---
+++ command/InsertCharGlyph.java
@@ -0,0 +1,38 @@
+package command;
+
+import glyph.Character;
+import glyph.Glyph;
+import window.Window;
+
+public class InsertCharGlyph implements Command{
+    Window window;
+    Glyph glyph;
+    char character;
+    public InsertCharGlyph(Window w, char c){
+        window = w;
+        character = c;
+        glyph = new Character(c, w);
+    }
+
+    @Override
+    public boolean undoable() {
+        return true;
+    }
+
+    @Override
+    public void execute() {
+        InsertPoint.getInsertPoint().insertGlyph(glyph);
+    }
+
+    @Override
+    public void unexecute() {
+        Glyph parent = glyph.getParent();
+        parent.remove(glyph);
+    }
+
+    @Override
+    public Command clone(){
+        return new InsertCharGlyph(window, character);
+    }
+
+}
